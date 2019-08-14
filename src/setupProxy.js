ef3d20f8859ed6e0b94d17250dbe8abce96ec31c
huangche007 <hc386271623@163.com>
2019-08-14 23:38:25 +0800

    fetch封装及跨域设置

---
+++ src/setupProxy.js
@@ -0,0 +1,16 @@
+const proxy = require('http-proxy-middleware');
+module.exports = function (app) {
+    app.use(proxy('/api', {
+        target: 'http://localhost:8080/',
+        pathRewrite: {
+            "^/api": "/"
+        }
+    }));
+    app.use(proxy('/admin', {
+        target: 'http://localhost:8080/',
+        // pathRewrite: {
+        //     "^/admin": "/"
+        // }
+        changeOrigin: true
+    }));
+};
